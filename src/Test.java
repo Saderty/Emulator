d5e795f2c7e8fe9998989391233a41242b696bbb
Saderty <sbk1162011@mail.ru>
2018-10-10 14:13:27 +0300

    Add search process method

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,27 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        System.out.println(Interpreter.Translator("CPI 11"));
-        System.out.println(Interpreter.commandMatrix[14][15]);
-        System.out.println(Interpreter.commandMatrix[1][2]);
+    private boolean checkProcess(String CmdProcess) {
+        StringBuilder fullprocess = new StringBuilder();
+        boolean inputcmd;
+        try {
+            Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                fullprocess.append(line).append("\n");
+            }
+            input.close();
+        } catch (IOException ignored) {
+        }
+        String process = fullprocess.toString();
+        inputcmd = process.contains(CmdProcess);
+        return inputcmd;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Test().checkProcess("idea64"));
     }
 }
